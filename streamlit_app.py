150f8c8a0d3e9ff46f2b01e0bb3369b49ee4f0b7
gywns9559 <131231745+gywns9559@users.noreply.github.com>
2023-04-19 15:01:16 +0900

    Update streamlit_app.py
    imoticon add

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,8 @@ import streamlit
 
 streamlit.title('my parents new healthy Diner')
 
-streamlit.header('Breakfast menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boild Free-Range Egg')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
